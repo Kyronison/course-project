3fc995705c889a10fd13c3ae0884389ba34f90e6
Войцев Александр Валерьевич <avvoitsev@edu.hse.ru>
2025-04-18 22:51:17 +0300

    routes new docstring and comments

--- app/routes/correlation_routes.py
+++ app/routes/correlation_routes.py
@@ -7,6 +7,20 @@ correlation_bp = Blueprint("correlation_bp", __name__)
 
 @correlation_bp.route("/yfinance/compare", methods=["GET"])
 def yfinance_compare():
+    """
+    Эндпоинт для расчета и предоставления данных корреляции между
+    указанным сектором акций и указанной криптовалютой.
+
+    Ожидает GET-параметры 'sector' и 'crypto'.
+    Использует get_correlation_data для получения и обработки данных.
+
+    Returns:
+        JSON: Объект JSON, содержащий списки дат ('dates'), значений сектора
+              ('sector_values'), значений криптовалюты ('crypto_values'),
+              и рассчитанную корреляцию ('correlation').
+              Возвращает JSON с сообщением об ошибке и соответствующим кодом статуса (400 или 500)
+              в случае некорректных параметров или недостатка данных.
+    """
     sector = request.args.get('sector')
     crypto = request.args.get('crypto')
 
